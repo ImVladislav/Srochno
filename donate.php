f3f9acda8b1316c403e99e0eb9c4e5aff0eb4aa7
ImVladislav <nafanya102@gmail.com>
2025-06-25 16:50:24 +0300

    sdf

---
+++ donate.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html><html lang="en" class="donation-banner-iframe"><head>
+<meta charset="utf-8"><title>Donate</title><style>body{margin:0}</style>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+      <script src="js/jquery-1.10.2.min.js"></script>
+      <script src="js/polyfill.min.js"></script>
+      <script src="js/athena.js"></script>
+          <script src="js/donation-banner.min.js" type="text/javascript"></script>
+    </head>
+    <body class="js-ia-iframe" style="margin:0">
+          <link href="css/banner.css" rel="stylesheet" type="text/css">
+    <link href="css/chevron.css" rel="stylesheet" type="text/css">
+      <header id="donate_banner" class="js-donate-banner formdesign-psa" itemscope="" itemtype="http://schema.org/WPAdBlock" data-donationsourcedata="ctx=wb;uid=aa95950f5c0f7c82011612ef621068a9;ref=https%3A%2F%2Fweb.archive.org%2Fweb%2F20231112063650%2Fhttps%3A%2F%2Fretardtoken.vip%2F" data-exp="20250601JuneDeploy" data-variant="WBPSABanner" data-platform="wb" data-baseurl="https://archive.org" data-bannertemplate="psa" data-bannerbackground="default" data-snowfall="off" data-selectedtextcolor="#000000" data-selectedfillcolor="#F9BF3B" data-paymentoptionfillcolor="" data-paymentoptiontextcolor="" data-formbgcolor="#ffffff" data-formtextcolor="#000000" data-donatepageselectedtextcolor="#000000" data-donatepageselectedfillcolor="#F9BF3B" data-variantdollaramounts="[10, 15, 50]" data-debugmode="false" style="--donationBannerChevronBG: #F9BF3B;">
+                  <div class="ia-banner visible psa">
+            <div class="appeal-text" style="--donationBannerAppealTextColor: #000000;">
+              <div class="desktop-body-content">We keep the record straight by preserving government websites, news articles, historical data, and more. If you find our archive useful, please <a class="psa-cta button">chip in</a></div>
+              <div class="mobile-body-content">We keep the record straight by preserving government websites, news articles, historical data, and more. If you find our archive useful, please <a class="psa-cta button">chip in</a></div>
+            </div>
+            <form id="donate-close-button" class="banner-close" aria-label="Close banner">
+              <span class="sr-only">Close banner</span>
+              <fieldset>
+                <button aria-label="Close Banner" type="submit"></button>
+              </fieldset>
+            </form>
+            <div style="display: none;">      <div class="donation-form-container">
+        <donation-form-edit-donation amountoptions="[10, 15, 50]" defaultselectedamount="15" amounttitledisplaymode="slot" customamountmode="hide" customfeescheckboxmode="hide" frequencyselectionmode="hide" stepnumbermode="hidenumbers" class="minimal-widget focus-on-child-only" style="--paymentButtonSelectedFontColor: #000000;--paymentButtonSelectedColor: #F9BF3B;--formSectionTextColor: #000000;">
+          <div slot="edit-donation-amount-title">
+            <p style="font-size: 1.8rem; line-height: calc(1.2rem * 2); margin: 0;"><b>Can You Chip In?</b> (USD)</p>
+          </div>
+        </donation-form-edit-donation>
+      </div>
+    </div>
+          </div>
+              </header>
+          
+  
+</body></html>
